4aa73cfa94451c3c76fef3472f9dc88fbcc06414
Satoshi Tanda <tanda.sat@gmail.com>
2024-02-19 19:54:03 -0800

    example

--- hypervisor/src/main.rs
+++ hypervisor/src/main.rs
@@ -13,9 +13,13 @@ mod serial_logger;
 mod switch_stack;
 mod x86_instructions;
 
-use core::arch::global_asm;
+use core::{arch::global_asm, ffi::c_void};
 use log::{debug, error, info};
-use uefi::prelude::*;
+use uefi::{
+    prelude::*,
+    proto::{loaded_image::LoadedImage, pi::mp::MpServices},
+    table::boot::{OpenProtocolAttributes, OpenProtocolParams},
+};
 use x86::{cpuid::cpuid, current::paging::BASE_PAGE_SIZE};
 
 use crate::{
@@ -40,6 +44,76 @@ fn main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
         return Status::ABORTED;
     }
 
+    let (image_base, image_size) = {
+        let bs = system_table.boot_services();
+        let loaded_image = bs
+            .open_protocol_exclusive::<LoadedImage>(bs.image_handle())
+            .unwrap();
+        loaded_image.info()
+    };
+
+    let image_base = image_base as usize;
+    let image_range = image_base..image_base + image_size as usize;
+    debug!("Image range: {image_range:#x?}");
+
+    // Prevent relocation by zapping the Relocation Table in the PE header. UEFI
+    // keeps the list of runtime drivers and applies patches into their code and
+    // data according with relocation information, as address translation switches
+    // from physical-mode to virtual-mode when the OS starts. This causes a problem
+    // with us because the host part keeps running under physical-mode, as the
+    // host has its own page tables. Relocation ends up breaking the host code.
+    // The easiest way is prevent this from happening is to nullify the relocation
+    // table.
+    unsafe {
+        *((image_base + 0x128) as *mut u32) = 0;
+        *((image_base + 0x12c) as *mut u32) = 0;
+    }
+
+    start_hypervisor_on_all_processors(&system_table);
+
+    Status::SUCCESS
+}
+
+/// Checks if this hypervisor is already installed.
+fn is_hlat_hypervisor_present() -> bool {
+    let regs = cpuid!(CPUID_VENDOR_AND_MAX_FUNCTIONS);
+    (regs.ebx == regs.ecx) && (regs.ecx == regs.edx) && (regs.edx == HLAT_VENDOR_NAME)
+}
+
+fn start_hypervisor_on_all_processors(system_table: &SystemTable<Boot>) {
+    let bs = system_table.boot_services();
+    let mp = unsafe {
+        bs.open_protocol::<MpServices>(
+            OpenProtocolParams {
+                handle: bs.get_handle_for_protocol::<MpServices>().unwrap(),
+                agent: bs.image_handle(),
+                controller: None,
+            },
+            OpenProtocolAttributes::GetProtocol,
+        )
+    }
+    .unwrap();
+
+    if mp.get_number_of_processors().unwrap().enabled == 1 {
+        start_hypervisor();
+    } else {
+        start_hypervisor();
+        mp.startup_all_aps(
+            true,
+            start_hypervisor_on_ap,
+            core::ptr::null_mut(),
+            None,
+            None,
+        )
+        .unwrap();
+    }
+}
+
+extern "efiapi" fn start_hypervisor_on_ap(_context: *mut c_void) {
+    start_hypervisor();
+}
+
+fn start_hypervisor() {
     // Capture the register values to be used as an initial state of the VM.
     let mut regs = GuestRegisters::default();
     unsafe { capture_registers(&mut regs) };
@@ -49,16 +123,9 @@ fn main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     // installing the hypervisor.
     if !is_hlat_hypervisor_present() {
         debug!("Virtualizing the system");
-        virtualize_system(&regs, &system_table);
+        virtualize_system(&regs);
     }
     info!("The HLAT hypervisor has been installed successfully🔥");
-    Status::SUCCESS
-}
-
-/// Checks if this hypervisor is already installed.
-fn is_hlat_hypervisor_present() -> bool {
-    let regs = cpuid!(CPUID_VENDOR_AND_MAX_FUNCTIONS);
-    (regs.ebx == regs.ecx) && (regs.ecx == regs.edx) && (regs.edx == HLAT_VENDOR_NAME)
 }
 
 extern "efiapi" {
--- hypervisor/src/switch_stack.rs
+++ hypervisor/src/switch_stack.rs
@@ -1,37 +1,11 @@
 use alloc::alloc::handle_alloc_error;
 use core::{alloc::Layout, arch::global_asm};
 use log::debug;
-use uefi::{
-    proto::loaded_image::LoadedImage,
-    table::{Boot, SystemTable},
-};
 
 use crate::{hypervisor::start_hypervisor, GuestRegisters, Page};
 
 /// Installs the hypervisor on the current processor.
-pub(crate) fn virtualize_system(regs: &GuestRegisters, system_table: &SystemTable<Boot>) -> ! {
-    let bs = system_table.boot_services();
-    let loaded_image = bs
-        .open_protocol_exclusive::<LoadedImage>(bs.image_handle())
-        .unwrap();
-    let (image_base, image_size) = loaded_image.info();
-    let image_base = image_base as usize;
-    let image_range = image_base..image_base + image_size as usize;
-    debug!("Image range: {image_range:#x?}");
-
-    // Prevent relocation by zapping the Relocation Table in the PE header. UEFI
-    // keeps the list of runtime drivers and applies patches into their code and
-    // data according with relocation information, as address translation switches
-    // from physical-mode to virtual-mode when the OS starts. This causes a problem
-    // with us because the host part keeps running under physical-mode, as the
-    // host has its own page tables. Relocation ends up breaking the host code.
-    // The easiest way is prevent this from happening is to nullify the relocation
-    // table.
-    unsafe {
-        *((image_base + 0x128) as *mut u32) = 0;
-        *((image_base + 0x12c) as *mut u32) = 0;
-    }
-
+pub(crate) fn virtualize_system(regs: &GuestRegisters) -> ! {
     // Allocate separate stack space. This is never freed.
     let layout = Layout::array::<Page>(0x10).unwrap();
     let stack = unsafe { alloc::alloc::alloc_zeroed(layout) };
